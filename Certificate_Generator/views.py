e557fe7dd161d511e343af732bd862b944ac16f7
Aniket Raj Singh <mohitrajingh223@gmail.com>
2022-09-19 23:27:57 +0530

    Gender Option Added

--- Certificate_Generator/views.py
+++ Certificate_Generator/views.py
@@ -8,21 +8,32 @@ def certificates(request):
     certback1='https://raw.githubusercontent.com/AniketRajSingh/Certificate-Generator/aniket_master/static/admin/img/Certificate_template_1.png'
     certback2='https://raw.githubusercontent.com/AniketRajSingh/Certificate-Generator/aniket_master/static/admin/img/Certificate_template_2.png'
     certback3='https://raw.githubusercontent.com/AniketRajSingh/Certificate-Generator/aniket_master/static/admin/img/Certificate_template_3.png'
-    param={'name':'Aniket','middlename':'Raj','sirname':'Singh','language':'Django','cert_background':certback1,'certback1':certback1,'certback2':certback2,'certback3':certback3}
+    param={'name':'Aniket','middlename':'Raj','sirname':'Singh','language':'Django','cert_background':certback1,'certback1':certback1,'certback2':certback2,'certback3':certback3,'gender':'his/her','gender1':'him/her','gender2':'Mr/Miss'}
     firstn=request.POST.get('fname','Aniket')
     middlen=request.POST.get('mname','Raj')
     lastn=request.POST.get('lname','Singh')
     langn=request.POST.get('lang','Django')
+    gender=request.POST.get('gender','his/her')
+    gender1=gender
+    gender2='Mr/Miss'
     certbackground=request.POST.get('certbackground','default')
-    if firstn == '' or langn=='' or certbackground=='default' :
+    if firstn == '' or langn=='' or certbackground=='default' or gender=='his/her':
         defaultans='One or Multiple important blanks that includes First name, Language and background selection has been left blank, thus the system is generating default output with default selections. '
         certbackground=certback1
     else:
         defaultans=''
 
+    if gender == 'male' :
+        gender='his'
+        gender1='him'
+        gender2='Mr'
+    elif gender =='female':
+        gender='her'
+        gender1='her'
+        gender2='Miss'
 
     certbackground=request.POST.get('certbackground',certback1)
-    param.update({'name':firstn,'middlename':middlen,'sirname':lastn,'language':langn,'cert_background':certbackground,'defaultans':defaultans})
+    param.update({'name':firstn,'middlename':middlen,'sirname':lastn,'language':langn,'cert_background':certbackground,'defaultans':defaultans,'gender':gender,'gender1':gender1,'gender2':gender2})
     return render(request,'certificates.html',param)
 def certform(request):
     certback1='https://raw.githubusercontent.com/AniketRajSingh/Certificate-Generator/aniket_master/static/admin/img/Certificate_template_1.png'
--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 Initial Phase Takes Input From Users and Generate a Certificate
 
  Future Goals - <!--Give Multiple Certificate Templates for Certificate Generation.-->
-                        -- Update - Multi Background Support Added.
+                    -- Update - Multi Background Support Added.
+                    -- Navigation Buttons Added
              - Add Multiple Other Pages to this Project Such as About & Contact 
              - Add Download Certificate Option 
              - Create Multiple Certificates using a csv file.(looping through the Dictionary and using Dict.update() function.)
@@ -17,4 +18,4 @@ Any Suggestions-
                Ph Number: +91 7248432377
                Whatsapp Link: https://api.whatsapp.com/send/?phone=7248432377&text&type=phone_number
 
-You Can Visit The App Here: certifyme-certifategenerator.herokuapp.com
+You Can Visit The App Here: certifyme-certificategenerator.herokuapp.com
